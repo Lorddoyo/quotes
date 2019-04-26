5234ef8b790abd37ecd9e66e57a9b374bf58d9ff
Lorddoyo <johnkojo7777@gmail.com>
2019-04-26 09:57:24 +0300

    added templateUrl in my component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: '<h1>My Quotes</h1>'
+                <p>This is a simple website for {{quote}}</p>,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
