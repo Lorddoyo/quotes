d507cf477238d573c48fc0df045065bdea43b2f1
Lorddoyo <johnkojo7777@gmail.com>
2019-04-26 10:47:12 +0300

    made a quote component

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  new Quote(1,'The greatest trick the Devil ever pulled was convincing the world he didn’t exist'),
+  new Quote(2,'Kama unapenda watoto wa shule ambia bibi yako avae school uniform'),
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
